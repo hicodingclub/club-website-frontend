031dd3a19bd8d96b8bb5c2f761a04f11d3c331b5
Xibin Liu <xibinliu@xibin-mac.local>
2020-05-29 00:52:51 -0700

    Use "hg angular-gen" for mra generation

--- front-adm/mra.sh
+++ front-adm/mra.sh
@@ -1,7 +1,6 @@
-
-mra -m emailing -o src/app ../../club-website-backend/models/emailing/model.js
-mra -m access -o src/app ../../club-website-backend/models/auth/model.access.js
-mra -m roles -o src/app ../../club-website-backend/models/auth/model.role.js
-mra -m files -o src/app ../../club-website-backend/models/files/sample.admin.js
-mra -m publicinfo -o src/app ../../club-website-backend/models/publicInfo/index.js
-mra -m teachforlife -o src/app ../../club-website-backend/models/teachforlife/index.js
+hg angular-gen -m emailing -o src/app ../../club-website-backend/models/emailing/model.js
+hg angular-gen -m access -o src/app ../../club-website-backend/models/auth/model.access.js
+hg angular-gen -m roles -o src/app ../../club-website-backend/models/auth/model.role.js
+hg angular-gen -m files -o src/app ../../club-website-backend/models/files/sample.admin.js
+hg angular-gen -m publicinfo -o src/app ../../club-website-backend/models/publicInfo/index.js
+hg angular-gen -m teachforlife -o src/app ../../club-website-backend/models/teachforlife/index.js
--- front-pub/mra.sh
+++ front-pub/mra.sh
@@ -1,2 +1,2 @@
-mra -m publicinfo -v public -o src/app ../../club-website-backend/models/publicInfo/index-public.js 
-mra -m teachforlife -v public -o src/app ../../club-website-backend/models/teachforlife/index-public.js
+hg angular-gen -m publicinfo -v public -o src/app ../../club-website-backend/models/publicInfo/index-public.js 
+hg angular-gen -m teachforlife -v public -o src/app ../../club-website-backend/models/teachforlife/index-public.js
