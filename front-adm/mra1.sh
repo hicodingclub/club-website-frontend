27551f8327b509b710b3ec8779d7bbb76c955dd0
Xibin Liu <xibinliu@xibin-mac.local>
2021-07-24 16:14:01 -0700

    mra1.sh

---
+++ front-adm/mra1.sh
@@ -0,0 +1,10 @@
+
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m emailing -o src/app ../../club-website-backend/node_modules/@hicoder/express-emailing/model.js
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m files -o src/app ../../club-website-backend/node_modules/@hicoder/express-file-server/model/sample.admin.js
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m access -o src/app ../../club-website-backend/node_modules/@hicoder/express-auth-server/authz/model.access.js
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m roles -o src/app ../../club-website-backend/node_modules/@hicoder/express-auth-server/models/model.role.default.js
+
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m publicinfo -o src/app ../../club-website-backend/models/publicInfo/index.js
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m teachforlife -o src/app ../../club-website-backend/models/teachforlife/index.js
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m summerprogram -o src/app ../../club-website-backend/models/summerprogram/index.js
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m websites -o src/app ../../club-website-backend/models/websites/index.js
---
+++ front-pub/mra1.sh
@@ -0,0 +1,4 @@
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m publicinfo -v public -o src/app ../../club-website-backend/models/publicInfo/index-public.js 
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m teachforlife -v public -o src/app ../../club-website-backend/models/teachforlife/index-public.js
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m summerprogram -v public -o src/app ../../club-website-backend/models/summerprogram/index-public.js
+~/dev/mean-rest/src/dev/angular-cli/bin/mra.js -m websites -v public -o src/app ../../club-website-backend/models/websites/index-public.js
